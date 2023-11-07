065bbf642aee4a880a00cef86d418e036b09a4d8
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-08 06:55:06 +0700

    fix: error when first creating app after clone/download

--- run.sh
+++ run.sh
@@ -7,16 +7,25 @@ echo "Compiling C++ program with g++"
 source_file="./main.cpp"
 
 # Output binary name (change this to your desired output name)
-output_binary="./output/main"
+output_binary="main"
+
+# Output folder (change this to your desired output folder)
+output_folder="./output/"
+
+# Create the output folder if it doesn't exist
+if [ ! -d "$output_folder" ]; then
+  echo "Creating output folder $output_folder..."
+  mkdir "$output_folder"
+fi
 
 # Compile the C++ program
-g++ -std=c++11 -Wall -Werror "$source_file" -o "$output_binary"
+g++ -std=c++11 -Wall -Werror "$source_file" -o "$output_folder$output_binary"
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then
   # Run the compiled binary
-  echo "Compilation successful. Running $output_binary..."
-  "$output_binary"
+  echo "Compilation successful. Running $output_folder$output_binary ..."
+  "$output_folder$output_binary"
 else
   echo "Compilation failed."
 fi
